78e2c6bc6ee3c222816811e5c9e7470faae8b486
Vish <vishvajitravalji@gmail.com>
2021-09-09 16:42:39 -0400

    test

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,15 @@
 const Sequelize = require("sequelize");
 
+<<<<<<< HEAD
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
 
+=======
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:qwerty@localhost:5432/messenger", {
+  logging: false
+});
+
+
+>>>>>>> 5243c98 (initial commit)
 module.exports = db;
